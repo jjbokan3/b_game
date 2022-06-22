3376762510fd3dcec5639b075b09f2b19dc6a2bd
Joseph Bokan <jjbokan3@gmail.com>
2022-06-22 12:45:10 -0400

    Created initial persons w/ attributes

--- Projects/b_game/test.py
+++ Projects/b_game/test.py
@@ -1,19 +1,3 @@
-# def big_function(num1: int, num2: int) -> int:
-#     """
-#     This function adds the two arguments
-#     Args:
-#         num1: First number
-#         num2: Second number
-#
-#     Returns:
-#         Sum of ''num1'' and ''num2''
-#     """
-#     return num1 + num2
-
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-ba = np.around(np.random.normal(70, 7, 20000))
-
-pd.Series(ba).hist()
+class Player():
+    def __init__(self):
+        s
